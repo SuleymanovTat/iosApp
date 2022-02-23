46b6c521139b639ed4b67c2ffd95d66ac5316d18
i.suleymanov <i.suleymanov@iportal.ru>
2022-02-23 19:29:48 +0500

    array IBOutlet

--- IosApp/ViewController.swift
+++ IosApp/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+ 
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func clickButton(_ sender: UIButton) {
+        sender.isHidden = true
+        print(sender.currentTitle)
+    }
 }
 
